7b122a556330110d3dc6b8ae7ef34239f27624e3
Arseniy <arsenshapar@gmail.com>
2022-11-30 22:11:27 +0200

    Added transform points

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from PIL import Image, ImageDraw
 
 
+def transform(point):
+    return point[1], -point[0] + 540
+
+
 def main():
-    with open("DS8.txt", "r") as file:
+    with open('DS8.txt', 'r') as file:
         lines = file.readlines()
 
     pointList = []
     for line in lines:
-        coords = line.strip().split(" ")
+        coords = line.strip().split(' ')
         x = int(coords[0])
         y = int(coords[1])
         point = (x, y)
-        pointList.append(point)
+        pointList.append(transform(point))
 
-    image = Image.new("1", (960, 540))
+    image = Image.new('1', (960, 540))
     draw = ImageDraw.Draw(image)
     for point in pointList:
-        draw.point(point, fill="white")
+        draw.point(point, fill='white')
+    image.save('result.png', 'PNG')
 
-    image.save("result.png", "PNG")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
